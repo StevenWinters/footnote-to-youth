a6b45bf2a584f04948db5a8c51b4856d8df36fd0
StevenWinters <mstevensoncc2007@gmail.com>
2024-12-05 08:29:10 +0800

    Fixed Responsiveness

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -4,23 +4,22 @@ import "../index.css";
 function MenuPage() {
   return (
     <section className="menu">
-      <div className="flex align--center menu__hero">
+      <div className="flex flex--column align--center menu__hero">
         <h1 className="menu__heading">
           Footnote
           <br /> to Youth
         </h1>
-        <div className="hero__container">
-          <span className="header__description">
-            a short story written by
-            <br /> José Garcia Villa
-          </span>
-          <Link to="home">
-            <div className="flex flex--column align--center menu__container">
-              <span className="menu__start">start</span>
-              <button className="btn btn--circle menu__btn"></button>
-            </div>
-          </Link>
-        </div>
+
+        <span className="header__description">
+          a short story written by
+          <br /> José Garcia Villa
+        </span>
+        <Link className="link" to="home">
+          <div className="flex flex--column align--center menu__container">
+            <span className="menu__start">start</span>
+            <button className="btn btn--circle menu__btn"></button>
+          </div>
+        </Link>
       </div>
     </section>
   );
